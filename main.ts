5a0cc5b4a1ba4234f9f86b594d662eeec9ca4517
nathan-haimanot <nathan.haimanot@mths.ca>
2024-11-24 10:54:42 -0500

    made microbit use bluetooth radios

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nathan Haimanot
+ * Created on: Nov 2024
+ * This program uses the bluetooth radios
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+radio.setGroup(1)
+
+// sonar setup and click a
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.clearScreen()
+
+    // if distance is < 10 send msg
+    if (distanceToObject < 10) {
+        radio.sendString("Too Close")
+        basic.showIcon(IconNames.Yes)
+
+    }
+
+    // recieve
+    radio.onReceivedString(function (receivedString) {
+        basic.clearScreen()
+        basic.showString(receivedString)
+        basic.showIcon(IconNames.Happy)
+    })
+
+})
